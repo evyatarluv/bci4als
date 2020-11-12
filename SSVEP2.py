142904b7778c11fff7bfb556adfd830a2b2b2fc0
luvaton <evyatarluv@gmail.com>
2020-11-12 11:11:08 +0200

    update get_eeg_data

--- SSVEP2.py
+++ SSVEP2.py
@@ -24,27 +24,19 @@ def get_eeg_data():
     # Get the params
     path = data_params['path']
     eeg_index = data_params['eeg_index']
-    labels_index = data_params['labels_index']
 
-
-    # TODO: get params from the function
+    # Get the xdf file
     data, header = pyxdf.load_xdf(path)
 
-    # Get the EEG data
-    channel_count = int(data[eeg_index]['info']['channel_count'][0])
-    eeg = pd.DataFrame(data=data[eeg_index]['time_series'],
-                       columns=list(range(1, channel_count + 1)))
-    eeg['time'] = data[eeg_index]['time_stamps']
-
-    print('g')
+    # Return the EEG data
+    return data[eeg_index]['time_series']
 
 
 def main():
 
     # Load the EEG data
     eeg = get_eeg_data()
-
-    print('h')
+    print('Loaded EEG data\nEEG data shape: {}'.format(eeg.shape))
 
 
 if __name__ == '__main__':
