08272785dc5e35624b964c2d46de71ee467d557b
luvaton <evyatarluv@gmail.com>
2020-11-12 14:20:41 +0200

    add channel names

--- SSVEP2.py
+++ SSVEP2.py
@@ -11,6 +11,8 @@ data_params = {
     'path': 'exp_example.xdf',
     'eeg_index': 1,
     'labels_index': 0,
+    'channel_names': ['Fp1', 'Fp2', 'C03', 'C04', 'P07', 'P08', 'O01', 'O02',
+                      'F07', 'F08', 'F03', 'F04', 'T07', 'T08', 'P03']
 }
 
 
@@ -28,15 +30,20 @@ def get_eeg_data():
     # Get the xdf file
     data, header = pyxdf.load_xdf(path)
 
-    # Return the EEG data
-    return data[eeg_index]['time_series']
+    # Get the EEG data & add channel names
+    eeg_data = data[eeg_index]['time_series']
+    eeg_data = pd.DataFrame(data=eeg_data, columns=data_params['channel_names'])
+
+    # Debug print
+    print('EEG data loaded\nData shape: {}'.format(eeg_data.shape))
+    return eeg_data
 
 
 def main():
 
     # Load the EEG data
     eeg = get_eeg_data()
-    print('Loaded EEG data\nEEG data shape: {}'.format(eeg.shape))
+
 
 
 if __name__ == '__main__':
