e85c49102f13ec836bded3132473b2c1d6b85ce6
luvaton <evyatarluv@gmail.com>
2020-11-12 15:33:18 +0200

    fix bugs

--- SSVEP2.py
+++ SSVEP2.py
@@ -8,11 +8,10 @@ Preprocessing of EEG data
 """
 
 data_params = {
-    'path': 'exp_example.xdf',
-    'eeg_index': 1,
+    'path': 'EEG.xdf',
     'channel_names': ['Fp1', 'Fp2', 'C03', 'C04', 'P07', 'P08', 'O01', 'O02',
                       'F07', 'F08', 'F03', 'F04', 'T07', 'T08', 'P03'],
-    'sample_freq': 120,  # TODO: Find the correct sample freq in OpenBCI
+    'sample_freq': None
 }
 
 filter_params = {
@@ -30,21 +29,29 @@ def get_eeg_data():
 
     # Get the params
     path = data_params['path']
-    eeg_index = data_params['eeg_index']
 
     # Get the xdf file
     data, header = pyxdf.load_xdf(path)
 
     # Get the EEG data
-    eeg_data = data[eeg_index]['time_series']
+    eeg_data = None
 
-    # Update channels name
-    # TODO: Update this according to OpenBCI setup
-    # eeg_data = np.delete(eeg_data, obj=0, axis=1)  # Delete the first channel
-    # eeg_data = pd.DataFrame(data=eeg_data, columns=data_params['channel_names'])  # Add channels names
+    for stream in data:
+        if 'EEG' in stream['info']['type']:
+
+            eeg_data = stream['time_series']  # get the data
+            data_params['sample_freq'] = stream['info']['nominal_srate'][0]  # update the sample rate
+
+    # Raise error if EEg stream wasn't found
+    if eeg_data is None:
+        raise NameError('EEG stream was not found in the xdf file')
+
+    # Remove the first channel
+    eeg_data = np.delete(eeg_data, obj=0, axis=1)
 
     # Debug print
     print('EEG data loaded\nData shape: {}'.format(eeg_data.shape))
+
     return eeg_data
 
 
