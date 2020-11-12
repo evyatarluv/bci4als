74f59352a133c6cae5ae60dde2f79008249b532e
luvaton <evyatarluv@gmail.com>
2020-11-12 10:41:49 +0200

    update get_eeg_data

--- SSVEP2.py
+++ SSVEP2.py
@@ -1,28 +1,42 @@
 import mne
 import pyxdf
+import pandas as pd
+import numpy as np
 
 """
 Preprocessing of EEG data
 """
 
 data_params = {
-    'path': 'EEG.xdf',
+    'path': 'exp_example.xdf',
     'eeg_index': 1,
     'labels_index': 0,
 }
 
 
 def get_eeg_data():
+
     """
 
     :return:
     """
 
+    # Get the params
+    path = data_params['path']
+    eeg_index = data_params['eeg_index']
+    labels_index = data_params['labels_index']
+
+
     # TODO: get params from the function
-    data, header = pyxdf.load_xdf('exp_example.xdf')
+    data, header = pyxdf.load_xdf(path)
 
     # Get the EEG data
-    data[1]
+    channel_count = int(data[eeg_index]['info']['channel_count'][0])
+    eeg = pd.DataFrame(data=data[eeg_index]['time_series'],
+                       columns=list(range(1, channel_count + 1)))
+    eeg['time'] = data[eeg_index]['time_stamps']
+
+    print('g')
 
 
 def main():
