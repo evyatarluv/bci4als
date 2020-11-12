d96f2c4e43be536800f883abb65240fa5f01a33e
luvaton <evyatarluv@gmail.com>
2020-11-12 10:07:20 +0200

    add SSVEP2

--- SSVEP2.py
+++ SSVEP2.py
@@ -5,3 +5,15 @@ import pyxdf
 Preprocessing of EEG data
 """
 
+data_path = 'EEG.xdf'
+
+def main():
+
+    # Load the EEG data
+    eeg = pyxdf.load_xdf(data_path)
+
+    print('h')
+
+if __name__ == '__main__':
+
+    main()
