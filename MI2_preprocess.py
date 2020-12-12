dded59ca7c8d12872e4771c668a6a04055e06301
luvaton <evyatarluv@gmail.com>
2020-12-12 20:59:51 +0200

    Add: load EEG data function

--- MI2_preprocess.py
+++ MI2_preprocess.py
@@ -1,9 +1,32 @@
 
 import os
+import pyxdf
+import numpy as np
 
 data_params = {
-    'record_folder': 'C:/Users/lenovo/Documents/CurrentStudy'
+    'record_folder': 'C:\\Users\\lenovo\\Documents\\CurrentStudy'  # path to the folder with all the subjects
 }
+
+
+def load_eeg_data(folder_path):
+
+    # Create the data path
+    path = os.path.join(folder_path, 'EEG.xdf')
+
+    # Get the xdf file
+    data, header = pyxdf.load_xdf(path, [{'type': 'EEG'}])
+
+    # Get the EEG data
+    eeg_data = data[0]['time_series']
+
+    # Remove the first channel
+    eeg_data = np.delete(eeg_data, obj=0, axis=1)
+
+    # Debug print
+    print('EEG data loaded\nData shape: {}'.format(eeg_data.shape))
+
+    return eeg_data
+
 
 
 def MI_preprocess():
@@ -14,7 +37,7 @@ def MI_preprocess():
     # For each subject clean the EEG data
     for s in subjects:
 
-        eeg_data = load_eeg_data()
+        eeg_data = load_eeg_data(os.path.join(data_params['record_folder'], s))
 
         eeg_data = filter_eeg_data(eeg_data)
 
