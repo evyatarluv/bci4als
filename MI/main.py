642c6592b3b1231962c19e5c9732193ec2d6e20a
luvaton <evyatarluv@gmail.com>
2020-12-16 11:24:32 +0200

    add data folder

--- MI/main.py
+++ MI/main.py
@@ -4,16 +4,16 @@ The main script which run all the MI steps
 """
 
 from MI.MI1_training import MI_training
-from MI.MI2_preprocess import  MI_preprocess
+from MI.MI2_preprocess import MI_preprocess
 from MI.MI3_segment_data import MI_segment_data
 from MI.MI4_extract_features import MI_extract_features
 
 
 if __name__ == '__main__':
 
-    # MI_training()
+    MI_training()
 
-    MI_preprocess()
+    # MI_preprocess()
 
-    MI_segment_data()
+    # MI_segment_data()
 
