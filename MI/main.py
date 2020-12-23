444f2091aa7da4b21dd1043aa49dcd5363077232
luvaton <evyatarluv@gmail.com>
2020-12-23 09:41:39 +0200

    Fix: MI2

--- MI/main.py
+++ MI/main.py
@@ -4,7 +4,7 @@ The main script which run all the MI steps
 """
 
 # from MI.MI1_record_experiment import MI_record
-# from MI.MI2_preprocess import MI_preprocess
+from MI.MI2_preprocess import MI_preprocess
 from MI.MI3_segment_data import MI_segment_data
 # from MI.MI4_extract_features import MI_extract_features
 # from MI.MI5_learn_model import MI5_learn_model
@@ -14,9 +14,9 @@ if __name__ == '__main__':
 
     # MI_record()
 
-    # MI_preprocess()
+    MI_preprocess()
 
-    MI_segment_data()
+    # MI_segment_data()
 
     # MI_extract_features(mode='cnn')
 
