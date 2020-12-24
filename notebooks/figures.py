020215e388d23c7004985cbc0c599146346b9489
luvaton <evyatarluv@gmail.com>
2020-12-24 10:47:15 +0200

    Add: start working on the pipeline notebook

---
+++ notebooks/figures.py
@@ -0,0 +1,26 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pyxdf
+from matplotlib.collections import LineCollection
+import mne
+import os
+
+data_path = '../data/evyatar/3'
+eeg_path = os.path.join(data_path, 'EEG.xdf')
+trials_path = os.path.join(data_path, 'EEG_trials.pickle')
+data = pyxdf.load_xdf(eeg_path, [{'type': 'EEG'}])[0][0]['time_series']
+data = np.delete(data, [0], axis=1)
+
+trials = pickle.load(open(trials_path, 'rb'))
+data = trials[50]
+ch_names = ['C03', 'C04', 'P07', 'P089', 'O01', 'O02', 'F07', 'F08', 'F03', 'F04', 'T07', 'T08', 'P03']
+# data = data[:, 3:5]
+# ch_names = ['C03', 'C04']
+s_rate = 125
+
+info = mne.create_info(ch_names, s_rate)
+raw_data = mne.io.RawArray(data.T, info)
+
+raw_data.plot()
