d0eab1482e742c1d68dbae424ed05383f5a9d049
noam <noamsi@post.bgu.ac.il>
2020-12-25 17:11:48 +0200

    Fix: Split files to MI5_train and MI6_test

---
+++ MI/MI6_test.py
@@ -0,0 +1,6 @@
+def MI_train():
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    MI_train()
--- MI/main.py
+++ MI/main.py
@@ -1,24 +1,42 @@
 
 """
-The main script which run all the MI steps
+The main script which runs all the MI steps
 """
 
 # from MI.MI1_record_experiment import MI_record
+from MI.MI1_record_experiment import MI_record
 from MI.MI2_preprocess import MI_preprocess
 from MI.MI3_segment_data import MI_segment_data
 # from MI.MI4_extract_features import MI_extract_features
-from MI.MI5_learn_model import MI5_learn_model
+from MI.MI5_train import MI5_learn_model
 
 
 if __name__ == '__main__':
 
-    # MI_record()
+    record = False
+    preprocess = False
+    segment = False
+    train = False
+    test = False
 
-    # MI_preprocess()
+    # Begin an experiment recording.
+    # Streams raw EEG data
+    if record:
+        MI_record()
 
-    # MI_segment_data()
+    # Apply filters and artifact removal
+    if preprocess:
+        MI_preprocess()
+
+    # Segment into annotated trials
+    if segment:
+        MI_segment_data()
+
 
     # MI_extract_features()
+    if train:
+        MI5_learn_model('same day', 'svm')
 
-    MI5_learn_model('same day', 'svm')
+    if test:
+        MI6_test()
 
