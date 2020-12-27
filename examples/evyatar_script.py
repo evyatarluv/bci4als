bf7d89d97c366ae067dd39cc42f2fc06e7ad243c
luvaton <evyatarluv@gmail.com>
2020-12-27 15:49:48 +0200

    Update: Report.ipynb ResNet

--- examples/evyatar_script.py
+++ examples/evyatar_script.py
@@ -1,3 +1,16 @@
-import bci4als.mi as mi
+# import bci4als.mi as mi
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-mi.assess_model.train('same day', 'svm')
+# mi.preprocess.preprocess()
+# mi.segment_data.segment_data()
+# mi.extract_features.extract()
+# mi.assess_model.train('same day', 'rf')
+
+results = pd.read_csv('../data/results.csv')
+sns.set_theme(style='whitegrid')
+sns.catplot(x='day', y='accuracy', hue='model', col='subject', data=results, kind='bar')
+plt.show()
+
+
--- src/bci4als/mi/__init__.py
+++ src/bci4als/mi/__init__.py
@@ -2,5 +2,6 @@ from .config import params
 from .record import record
 from .segment_data import segment_data
 from .extract_features import extract_features
+from.preprocess import preprocess
 from .assess_model import assess_model
 
