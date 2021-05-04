ccd6e49d4f12815ec730f9be1711efe230da1a59
luvaton <evyatarluv@gmail.com>
2021-05-04 16:11:14 +0300

    syntactic sugar of mouse control pipe

--- examples/control_mouse.py
+++ examples/control_mouse.py
@@ -1,19 +1,32 @@
+import pickle
 import sys
 import threading
 from PyQt5.QtWidgets import QApplication
 from bci4als.mouse import VirtualMouse, MouseConfig
 
 
+def predict_label(model):
+    # todo: fill up this function - ML model predict the label of the user
+    return 1
+
+
 def control_mouse(config: MouseConfig):
 
     virtual_mouse = VirtualMouse(config)
+    model = pickle.load(open(r'models/7/sgd.pkl', 'rb'))
 
     while True:
 
+        # Monitor the movement to indicate standstill
         virtual_mouse.monitor(r=25, counter_limit=5, interval=0.4)
 
-        action = config.get_action(label=1)  # todo: `label` arg need to be the ML model prediction
+        # Predict the label imagined by the user
+        label = predict_label(model)
 
+        # Convert the label to action according to current config
+        action = config.get_action(label=label)
+
+        # Execute the action
         virtual_mouse.execute(action=action)
 
 
